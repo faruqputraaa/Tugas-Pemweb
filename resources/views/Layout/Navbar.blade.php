c62e5529be0d3eebc3125cfee8ae4dba93f91a8e
MasDewaa <ramasyailana3@gmail.com>
2024-05-17 02:55:47 +0700

    FINAL UTS

--- resources/views/Layout/Navbar.blade.php
+++ resources/views/Layout/Navbar.blade.php
@@ -9,6 +9,7 @@
     <link rel="shortcut icon" href="{{ asset('assets/icons/logo.ico') }}">
     <title>Boikot Sampah</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
